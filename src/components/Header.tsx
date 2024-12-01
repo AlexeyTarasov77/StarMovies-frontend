f597199cd6fd332295aab38970266f3b6403090e
Liudmyla Makhynia <milam20090929@gmail.com>
2024-12-01 21:35:43 +0200

    add input styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export function Header(){
                 </div>
 
                 <div className="flex ml-16 bg-white/50 rounded-2xl">
-                    <input type="text" className="h-14 w-80 rounded-2xl bg-white/0 "/> 
+                    <input type="text" className="h-14 w-80 rounded-2xl bg-white/0 focus:outline-none text-xl "/> 
                     {/* <img src="../assets/search.jpg" alt="" /> */}
                     <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-16 w-8 pr-0.5">
                         <path d="M11 19.5C15.4183 19.5 19 15.9183 19 11.5C19 7.08172 15.4183 3.5 11 3.5C6.58172 3.5 3 7.08172 3 11.5C3 15.9183 6.58172 19.5 11 19.5Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
