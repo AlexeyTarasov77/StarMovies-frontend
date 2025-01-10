4ab1ef0971b337ae9e1f6dd266cb73eecfef5765
Марія Драгнєва <dragnevamasa95@gmail.com>
2025-01-10 22:50:46 +0200

    Reviews added stars

--- src/pages/film-detail/ui/film-in-list.tsx
+++ src/pages/film-detail/ui/film-in-list.tsx
@@ -42,35 +42,37 @@ export interface IReview {
   id: number;
   rating: number;
   comment: string | null;
-  stars: string;
   movieId: number;
   userId: number;
   createdAt: Date;
   updatedAt: Date;
 }
-
-export function Review ({ reviewData }: { reviewData: IReview }) {
+export function Review({ reviewData }: { reviewData: IReview }) {
+  const getStars = (rating: number): string => {
+    const allStars = 10
+    const filledStars = "★".repeat(rating)
+    const emptyStars = "☆".repeat(allStars - rating)
+    return filledStars + emptyStars
+  }
+  
   return (
     <div className="flex flex-col p-3">
-      <div className="flex flex-col bg-customGray " >
-            <div className="flex justify-between flex-row items-baseline border-b-2 p-2 mb-2">
-              <h1 className="text-lg font-bold">Name</h1>
-              <p className="text-sm text-gray-400 pl-2">
-                {reviewData.createdAt.toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </p>
-            </div>
-            <div className="p-2">
-              <p>{reviewData.comment}</p>
-              <div className="flex flex-row gap-2">
-              <p>{reviewData.stars}</p>
-              <p>{reviewData.rating}</p></div>
-            </div>
+      <div className="flex flex-col bg-customGray">
+        <div className="flex justify-between flex-row items-baseline border-b-2 p-2 mb-2">
+          <h1 className="text-lg font-bold">Name</h1>
+          <p className="text-sm text-gray-400 pl-2">
+            {reviewData.createdAt.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </p>
+        </div>
+        <div className="p-2">
+          <p>{reviewData.comment}</p>
+          <p className="text-lg">{getStars(reviewData.rating)}</p>
+        </div>
       </div>
     </div>
   );
 }
-
